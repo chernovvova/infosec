833a78a643339ea2f5d99b43d68a4b8853d3e54e
Vladimir Chernov <chernovvova2023@yandex.ru>
2026-04-19 21:28:09 +0300

    feat: tests for Zp

--- tests/finite_field_test.py
+++ tests/finite_field_test.py
@@ -6,8 +6,43 @@ from entities.finite_field import Zp
 @pytest.mark.parametrize(
     ['a', 'b', 'expected'],
     (
-        (Zp(1, 5), Zp(3, 5), Zp(4, 5)),
+        (Zp(1, 5), Zp(1, 5), True),
+        (Zp(2, 5), Zp(3, 5), False),
+        (Zp(1, 3), Zp(1, 4), False),
     ),
 )
-def test_zp_add(a, b, expected):
-    assert a + b == expected
+def test_zp_eq(a: Zp, b: Zp, expected: Zp) -> None:
+    assert bool(a == b) == expected
+
+
+@pytest.mark.parametrize(
+    ['a', 'b', 'expected'],
+    (
+        (Zp(1, 5), Zp(3, 5), Zp(4, 5)),
+        (Zp(2, 5), Zp(3, 5), Zp(0, 5)),
+    ),
+)
+def test_zp_add(a: Zp, b: Zp, expected: Zp) -> None:
+    assert (a + b) == expected
+
+
+@pytest.mark.parametrize(
+    ['a', 'b', 'expected'],
+    (
+        (Zp(4, 5), Zp(3, 5), Zp(1, 5)),
+        (Zp(-1, 5), Zp(4, 5), Zp(0, 5)),
+    ),
+)
+def test_zp_sub(a: Zp, b: Zp, expected: Zp) -> None:
+    assert (a - b) == expected
+
+
+@pytest.mark.parametrize(
+    ['a', 'b', 'expected'],
+    (
+        (Zp(4, 5), Zp(3, 5), Zp(2, 5)),
+        (Zp(-8, 5), Zp(4, 5), Zp(3, 5)),
+    ),
+)
+def test_zp_sub(a: Zp, b: Zp, expected: Zp) -> None:
+    assert (a * b) == expected
